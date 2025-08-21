2d3f8e635ab51827d388e0c4c53ed61362abafdb
rogercorellana <rcamacho9805@gmail.com>
2025-08-21 19:24:16 -0300

    Edicion de archivos doc

---
+++ Desarrollo/growshiUI/Login/Login.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace growshiUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.textBoxUsuario = new MetroFramework.Controls.MetroTextBox();
+            this.textBoxContraseña = new MetroFramework.Controls.MetroTextBox();
+            this.buttonIniciarSesion = new MetroFramework.Controls.MetroButton();
+            this.linkLabelOlvideMiContraseña = new MetroFramework.Controls.MetroLink();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(279, 26);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(168, 23);
+            this.textBoxUsuario.TabIndex = 0;
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.Location = new System.Drawing.Point(279, 76);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.PasswordChar = '*';
+            this.textBoxContraseña.Size = new System.Drawing.Size(168, 23);
+            this.textBoxContraseña.TabIndex = 1;
+            // 
+            // buttonIniciarSesion
+            // 
+            this.buttonIniciarSesion.Location = new System.Drawing.Point(255, 128);
+            this.buttonIniciarSesion.Name = "buttonIniciarSesion";
+            this.buttonIniciarSesion.Size = new System.Drawing.Size(192, 23);
+            this.buttonIniciarSesion.Style = MetroFramework.MetroColorStyle.Green;
+            this.buttonIniciarSesion.TabIndex = 2;
+            this.buttonIniciarSesion.Text = "Iniciar Sesion";
+            this.buttonIniciarSesion.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // linkLabelOlvideMiContraseña
+            // 
+            this.linkLabelOlvideMiContraseña.Location = new System.Drawing.Point(317, 157);
+            this.linkLabelOlvideMiContraseña.Name = "linkLabelOlvideMiContraseña";
+            this.linkLabelOlvideMiContraseña.Size = new System.Drawing.Size(130, 21);
+            this.linkLabelOlvideMiContraseña.TabIndex = 8;
+            this.linkLabelOlvideMiContraseña.Text = "Olvide Mi Contraseña";
+            this.linkLabelOlvideMiContraseña.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.metroPanel1.Controls.Add(this.pictureBox3);
+            this.metroPanel1.Controls.Add(this.pictureBox2);
+            this.metroPanel1.Controls.Add(this.pictureBox1);
+            this.metroPanel1.Controls.Add(this.linkLabelOlvideMiContraseña);
+            this.metroPanel1.Controls.Add(this.textBoxUsuario);
+            this.metroPanel1.Controls.Add(this.textBoxContraseña);
+            this.metroPanel1.Controls.Add(this.buttonIniciarSesion);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(17, 37);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(477, 215);
+            this.metroPanel1.TabIndex = 9;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(255, 76);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 23);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(255, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 23);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 266);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.metroPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox textBoxUsuario;
+        private MetroFramework.Controls.MetroTextBox textBoxContraseña;
+        private MetroFramework.Controls.MetroButton buttonIniciarSesion;
+        private MetroFramework.Controls.MetroLink linkLabelOlvideMiContraseña;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Desarrollo/growshiUI/Login/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace growshiUI
+{
+    public partial class Login : MetroFramework.Forms.MetroForm
+    {
+        public Login()
+        {
+
+            InitializeComponent();
+            this.ActiveControl = null;
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+          
+
+            textBoxUsuario.PromptText = "Usuario o Email";
+            textBoxContraseña.PromptText = "Contraseña";
+            textBoxUsuario.Text = null;
+            textBoxContraseña.Text = null;
+
+            // 3. Quitamos el foco inicial.
+            this.ActiveControl = null;
+
+
+
+        }
+
+
+    }
+}
