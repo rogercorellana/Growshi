c45d8b5451d40eefe8a74ca7c28a164208aa6fe9
rogercorellana <rcamacho9805@gmail.com>
2025-08-25 18:50:15 -0300

    Diagrama DER - Login

---
+++ Desarrollo/BLL/UsuarioBLL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+        #region Patron Singleton 
+        private static UsuarioBLL _instancia;
+        private UsuarioBLL() { }
+        public static UsuarioBLL GetInstance
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                            _instancia = new UsuarioBLL();
+
+                }
+                return _instancia;
+            }
+        }
+        #endregion
+
+
+        public void iniciarSesion(string usuario, string contraseña)
+        {
+
+        }
+
+    }
+}
---
+++ Desarrollo/Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Interfaces")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interfaces")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("899012e8-79f6-498b-be42-a2f3653ef64b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
